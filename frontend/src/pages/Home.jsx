0c726e10b53fc2366ccfb5a3d51970dc0b31e95a
abhishek2k4 <iabbyshek@gmail.com>
2025-12-30 14:53:28 +0530

    contextapi setup done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { NoteContext } from '../context/NoteContext.jsx';
 
 function Home() {
+  const {notes,loading} = React.useContext(NoteContext);
+  console.log(notes)
   return (
     <div>Home</div>
   )
